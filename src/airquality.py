3da9352000369d3a1bd1a0507831250fb56da8fe
Efe Incekas <96147676+Mejdruirb@users.noreply.github.com>
2022-01-04 12:55:37 +0100

    Create airquality.py

---
+++ src/airquality.py
@@ -0,0 +1,17 @@
+import serial, time
+from Adafruit_IO import Client
+aio = Client(mejdruirb, aio_sHMT48MPzFRkUxN92pxDqOxYzqU5)
+
+ser = serial.Serial("/dev/ttyUSB0")
+
+while True:
+  data = []
+  for index in range(0,10):
+    datum = ser.read()
+    data.append(datum)
+    
+  pmtwofive = int.from_bytes(b’’.join(data[2:4]), byteorder=’little’) / 10
+  aio.send("kingswoodtwofive", pmtwofive)
+  pmten = int.from_bytes(b’’.join(data[4:6]), byteorder=’little’) / 10
+  aio.send("kingswoodten", pmten)
+  time.sleep(10)
