c74d7971c4db9035c4f4f80c71560f828d10dda3
Efe Incekas <96147676+Mejdruirb@users.noreply.github.com>
2022-01-09 22:26:10 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,6 +6,8 @@ cd ../
 sudo apt-get update
 sudo apt-get install -y python3 python3-pip python3-dev libffi-dev libssl-dev
 
+pip3 install pyserial adafruit-io
+
 curl -sSL https://get.docker.com | sh
 sudo usermod -aG docker ${USER}
 sudo pip3 install docker-compose
